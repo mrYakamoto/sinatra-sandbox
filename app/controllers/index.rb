899bf9b350dffaf368fcf427b508fce6e446d2f1
Mia Lehrer <mia.lehrer@gmail.com>
2015-12-28 17:18:21 -0800

    Make some forms

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,19 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "[LOG] Responding to a GET request for /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
+end
+
+get '/sandbox' do
+  puts "[LOG] Responding to a GET request for /sandbox"
+  puts "[LOG] Params: #{params.inspect}"
+  @var = params["second_input"]
+  erb :get_sandbox
+end
+
+post '/sandbox' do
+  puts "[LOG] Responding to a POST request to /sandbox"
+  puts "[LOG] Params: #{params.inspect}"
+  @thing = params["first_input"]
+  erb :post_sandbox
 end
